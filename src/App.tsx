fac571672ba2fff14e23292bafe5f08ae395dce7
Conor <conorhamilton@icloud.com>
2024-03-22 11:43:05 +0000

    installing tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import "./styles/index.css";
+
 function App() {
-  return <h1>Hello Project 3!</h1>
+  return (
+    <>
+      <h2 className="text-7xl font-bold underline">Hello world!</h2>;
+    </>
+  );
 }
 
-export default App
+export default App;
