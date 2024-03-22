45bedf3583c4ae97b03b7cb409c3d19150df0752
Catherine Brett <catherinejanebrett@icloud.com>
2024-03-22 16:05:51 +0000

    Changed some file names, wrote ITip interface and preliminary TipsList and Tips components

---
+++ src/components/TipsList.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Tip from "./Tip";
+import { ITip } from "../interfaces/tip";
+
+type Tips = null | Array<ITip>;
+
+function TipsList() {
+  const [tips, setTips] = React.useState<Tips>(null);
+
+  React.useEffect(() => {
+    async function fetchTips() {
+      const resp = await fetch("/api/tips");
+      const data = await resp.json();
+      setTips(data);
+    }
+    fetchTips();
+  }, []);
+
+  console.log("Here are all the tips we have fetched:");
+  console.log(tips);
+
+  
+  // TODO: Add Tailwind/CSS to this
+  return (
+    <section>
+      <div>
+        <div>
+          {tips?.map((tip) => {
+            return <Tip key={tip._id} {...tip} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TipsList
