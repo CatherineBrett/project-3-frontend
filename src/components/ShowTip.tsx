501b86e7f7b937755a7182ce7c43af8b6e5b4b49
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-03-26 16:39:29 +0000

    delete button styling

--- src/components/ShowTip.tsx
+++ src/components/ShowTip.tsx
@@ -50,13 +50,13 @@ export default function ShowTip({ user }: { user: null | IUser }) {
         <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 py-2">
           <Card key={tip._id} {...tip} />
         </div>
-        <div className="flex-initial w-full md:w-1/2 lg:w-1/3">
+        <div className="flex-initial w-full md:w-1/2 lg:w-1/3 flex flex-col items-center">
           <div className="bg-white p-4 rounded-lg shadow-md">
             <h2 className="text-lg font-semibold">About the User</h2>
             <p>{tip.user?.bio}</p>
           </div>
+          {tip && (user?._id === tip.user._id || user?._id === "66029050610777603484521b") && <div className="mt-4"><button onClick={deleteTip} className="bg-red-500 text-white px-10 py-2 rounded-full hover:bg-red-400 mt-12">Delete Tip</button></div>}
         </div>
-        {tip && (user?._id === tip.user._id || user?._id === "66029050610777603484521b") && <button onClick={deleteTip} className="bg-red-500 text-white px-5 py-2 rounded-full hover:bg-red-400">Delete Tip</button>}
       </div>
     </section>
   );
