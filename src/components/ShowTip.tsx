024452959e389a6564971624398a58250bcbbbf6
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-03-26 12:41:30 +0000

    local merge

--- src/components/ShowTip.tsx
+++ src/components/ShowTip.tsx
@@ -14,6 +14,7 @@ export default function ShowTip() {
         const response = await fetch(`/api/tips/${tipId}`);
         const data: ITip = await response.json();
         setTip(data);
+
       } catch (error) {
         console.error("Error fetching tip:", error);
       }
@@ -31,10 +32,10 @@ export default function ShowTip() {
 
   return (
     <section className="container mx-auto pt-24 flex justify-center gap-8">
-  <div className="flex flex-wrap justify-center lg:flex-nowrap gap-8">
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 py-2">
-      <Card key={tip._id} {...tip} />
-    </div>
+      <div className="flex flex-wrap justify-center lg:flex-nowrap gap-8">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 py-2">
+          <Card key={tip._id} {...tip} />
+        </div>
         <div className="flex-initial w-full md:w-1/2 lg:w-1/3">
           <div className="bg-white p-4 rounded-lg shadow-md">
             <h2 className="text-lg font-semibold">About the User</h2>
